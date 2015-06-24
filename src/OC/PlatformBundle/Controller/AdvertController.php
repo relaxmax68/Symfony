5d22951a3748df381daf1a16ac314c15c2af0904
relaxmax@spire <maxime.code@bbox.fr>
2015-06-24 17:08:14 +0200

    manipuler les sessions

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -5,7 +5,7 @@
 namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 
@@ -16,10 +16,17 @@ class AdvertController extends Controller
 		$content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig',array('nom'=>'Maxime'));
 	    return new Response($content);
     }
-    public function viewAction($id)
-    {
-  		return new JsonResponse(array('id' => $id));
-	}
+ 	public function viewAction($id, Request $request)
+	{
+	    // Récupération de la session
+	    $session = $request->getSession();
+	    // On récupère le contenu de la variable user_id
+	    $userId = $session->get('user_id');
+	    // On définit une nouvelle valeur pour cette variable user_id
+	    $session->set('user_id', 91);
+	    // On n'oublie pas de renvoyer une réponse
+	    return new Response("Je suis une page de test, je n'ai rien à dire");
+ 	}
 	public function viewSlugAction($slug, $year, $format)
     {
         return new Response("On pourrait afficher l'annonce correspondant au slug '".$slug."', créée en ".$year." et au format ".$format."."
