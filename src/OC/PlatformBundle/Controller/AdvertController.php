d875fa845df77a56d4abbdf5d2268c5518c8f040
relaxmax@spire <maxime.code@bbox.fr>
2015-06-24 17:31:12 +0200

    ajout des routes CRUD

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -6,15 +6,23 @@ namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 
 class AdvertController extends Controller
 {
-    public function indexAction()
+    public function indexAction($page)
     {
-		$content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig',array('nom'=>'Maxime'));
-	    return new Response($content);
+		// On ne sait pas combien de pages il y a
+	    // Mais on sait qu'une page doit être supérieure ou égale à 1
+	    if ($page < 1) {
+	      // On déclenche une exception NotFoundHttpException, cela va afficher
+	      // une page d'erreur 404 (qu'on pourra personnaliser plus tard d'ailleurs)
+	      throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
+	    }
+	    // Ici, on récupérera la liste des annonces, puis on la passera au template
+	    // Mais pour l'instant, on ne fait qu'appeler le template
+	    return $this->render('OCPlatformBundle:Advert:index.html.twig');
     }
 	public function viewAction($id)
 	{
@@ -22,19 +30,36 @@ class AdvertController extends Controller
 	}
 	public function addAction(Request $request)
 	{
-	    $session = $request->getSession();
-	    // Bien sûr, cette méthode devra réellement ajouter l'annonce
-	    // Mais faisons comme si c'était le cas
-	    $session->getFlashBag()->add('info', 'Annonce bien enregistrée');
-	    // Le « flashBag » est ce qui contient les messages flash dans la session
-	    // Il peut bien sûr contenir plusieurs messages :
-	    $session->getFlashBag()->add('info', 'Oui oui, il est bien enregistré !');
-	    // Puis on redirige vers la page de visualisation de cette annonce
-	    return $this->redirect($this->generateUrl('oc_platform_view', array('id' => 5)));
+    // La gestion d'un formulaire est particulière, mais l'idée est la suivante :
+    // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
+	    if ($request->isMethod('POST')) {
+	        // Ici, on s'occupera de la création et de la gestion du formulaire
+	        $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+	        // Puis on redirige vers la page de visualisation de cettte annonce
+	        return $this->redirect($this->generateUrl('oc_platform_view', array('id' => 5)));
+    	}
+	    // Si on n'est pas en POST, alors on affiche le formulaire
+	    return $this->render('OCPlatformBundle:Advert:add.html.twig');
 	}
 	public function viewSlugAction($slug, $year, $format)
     {
         return new Response("On pourrait afficher l'annonce correspondant au slug '".$slug."', créée en ".$year." et au format ".$format."."
         );
+    }
+    public function editAction($id, Request $request)
+    {
+	    // Ici, on récupérera l'annonce correspondante à $id
+	    // Même mécanisme que pour l'ajout
+	    if ($request->isMethod('POST')) {
+	        $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
+	        return $this->redirect($this->generateUrl('oc_platform_view', array('id' => 5)));
+	    }
+	    return $this->render('OCPlatformBundle:Advert:edit.html.twig');
+    }
+    public function deleteAction($id)
+    {
+	    // Ici, on récupérera l'annonce correspondant à $id
+	    // Ici, on gérera la suppression de l'annonce en question
+	    return $this->render('OCPlatformBundle:Advert:delete.html.twig');
     }
 }
